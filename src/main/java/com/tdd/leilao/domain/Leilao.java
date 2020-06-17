a5d09ceb34119266a019a4b3329a5b893722d9a3
Paulo H <phmarcusso1@hotmail.com>
2020-06-17 00:19:28 -0300

    implementacao de TDD para a classe Leilao

--- src/main/java/com/tdd/leilao/domain/Leilao.java
+++ src/main/java/com/tdd/leilao/domain/Leilao.java
@@ -8,23 +8,43 @@ public class Leilao {
 
 	private String descricao;
 	private List<Lance> lances;
-	
+
 	public Leilao(String descricao) {
 		this.descricao = descricao;
 		this.lances = new ArrayList<Lance>();
 	}
-	
+
 	public void propoe(Lance lance) {
-		lances.add(lance);
+
+		int total = qntDeLancesDo(lance.getUsuario());
+
+		if (lances.isEmpty() || (podeDarLance(lance.getUsuario()))) {
+			lances.add(lance);
+		}
 	}
-	
+
+	private boolean podeDarLance(Usuario usuario) {
+		return !ultimoLanceDado().getUsuario().equals(usuario) && qntDeLancesDo(usuario) < 5;
+	}
+
+	private int qntDeLancesDo(Usuario usuario) {
+		int total = 0;
+		for (Lance l : lances) {
+			if(l.getUsuario().equals(usuario)) total++;
+		}
+		return total;
+	}
+
+	private Lance ultimoLanceDado() {
+		return lances.get(lances.size() - 1);
+	}
+
 	public String getDescricao() {
 		return descricao;
 	}
-	
-	public List<Lance> getLances(){
+
+	public List<Lance> getLances() {
 		return Collections.unmodifiableList(lances);
 	}
-	
-	
+
 }
--- src/test/java/com/tdd/leilao/teste/domain/LeilaoTest.java
+++ src/test/java/com/tdd/leilao/teste/domain/LeilaoTest.java
@@ -1,5 +1,75 @@
 package com.tdd.leilao.teste.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+import com.tdd.leilao.domain.Lance;
+import com.tdd.leilao.domain.Leilao;
+import com.tdd.leilao.domain.Usuario;
+
 public class LeilaoTest {
 
+	@Test
+	public void deveReceberUmLance() {
+		
+		Leilao leilao = new Leilao("Playstation 5");
+		
+		//sem nenhum lance
+		assertEquals(0, leilao.getLances().size());
+		
+		//verificando a quantidade de lances e valor
+		leilao.propoe(new Lance(new Usuario("paulo"), 2000.0));		
+		assertEquals(1, leilao.getLances().size());
+		assertEquals(2000.0, leilao.getLances().get(0).getValor(), 0.0001);
+	}
+	
+	@Test
+	public void deveReceberVariosLances() {
+		
+		Leilao leilao = new Leilao("Playstation 5");
+		
+		leilao.propoe(new Lance(new Usuario("paulo"), 3000.0));
+		leilao.propoe(new Lance(new Usuario("pamela"), 4000.0));
+		
+		assertEquals(2, leilao.getLances().size());
+		assertEquals(3000.0, leilao.getLances().get(0).getValor(), 0.0001);
+		assertEquals(4000.0, leilao.getLances().get(1).getValor(), 0.0001);
+	}
+	
+	@Test
+	public void naoDeveAceitarDoisLancesSeguidosDoMesmoUsuario() {
+		
+		Leilao leilao = new Leilao("Playstation 5");
+		
+		leilao.propoe(new Lance(new Usuario("paulo"), 3000.0));
+		leilao.propoe(new Lance(new Usuario("paulo"), 4000.0));
+		
+		assertEquals(1, leilao.getLances().size());
+		assertEquals(3000.0, leilao.getLances().get(0).getValor());
+	}
+	
+	@Test
+	public void naoDeveAceitarCincoLancesDeUmMesmoUsuario() {
+		Leilao leilao = new Leilao("Playstation 5");
+		
+		leilao.propoe(new Lance(new Usuario("paulo"), 3000.0));
+		leilao.propoe(new Lance(new Usuario("pamela"), 4000.0));
+		leilao.propoe(new Lance(new Usuario("paulo"), 3000.0));
+		leilao.propoe(new Lance(new Usuario("pamela"), 4000.0));
+		leilao.propoe(new Lance(new Usuario("paulo"), 3000.0));
+		leilao.propoe(new Lance(new Usuario("pamela"), 4000.0));
+		leilao.propoe(new Lance(new Usuario("paulo"), 3000.0));
+		leilao.propoe(new Lance(new Usuario("pamela"), 4000.0));
+		leilao.propoe(new Lance(new Usuario("paulo"), 3000.0));
+		leilao.propoe(new Lance(new Usuario("pamela"), 4000.0));
+		
+		//deve ser ignorado
+		leilao.propoe(new Lance(new Usuario("paulo"), 3000.0));
+		
+		assertEquals(10, leilao.getLances().size());
+		assertEquals(4000.0, leilao.getLances().get(leilao.getLances().size()-1).getValor(), 0.0001);
+		
+		
+	}
 }
